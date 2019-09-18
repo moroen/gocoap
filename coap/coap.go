79d68abfa93dae10f8f8db0415420cd6bd7c9a94
moroen <moroen@gmail.com>
2019-09-19 01:43:43 +0200

    Barebone coap library

--- coap/coap.go
+++ coap/coap.go
@@ -1,49 +1,47 @@
-package main
-
-// #cgo pkg-config: python3
-// #define Py_LIMITED_API
-// #include <Python.h>
-// int PyArg_ParseTuple_LL(PyObject *, long long *, long long *);
-// int PyArg_ParseTuple_S(PyObject *, char *);
-// char * ParseStringArgument(PyObject *);
-import "C"
+package gocoap
 
 import (
 	"errors"
+	"fmt"
+
 	"time"
 
-	"github.com/moroen/canopus"
+	coap "github.com/dustin/go-coap"
+	"github.com/eriklupander/dtls"
 )
 
-type CoapRequest struct {
-	Gateway  string
-	Uri      string
-	Identity string
-	Passkey  string
-	Payload  string
+type RequestParams struct {
+	host    string
+	port    int
+	uri     string
+	id      string
+	key     string
+	req     coap.Message
+	payload string
 }
 
-type CoapResult struct {
-	msg canopus.MessagePayload
-	err error
-}
-
+// ErrorTimeout error
 var ErrorTimeout = errors.New("COAP Error: Connection timeout")
+
+// ErrorBadIdent error
 var ErrorBadIdent = errors.New("COAP DTLS Error: Wrong credentials?")
+
+// ErrorNoConfig error
 var ErrorNoConfig = errors.New("COAP Error: No config")
 
-func _getRequest(request CoapRequest, c chan CoapResult) {
+/*
+func _getRequest(URI string, c chan CoapResult) {
 
 	var result CoapResult
-	var conn canopus.Connection
-	var err error
 
-	if request.Identity != "" {
-		conn, err = canopus.DialDTLS(request.Gateway, request.Identity, request.Passkey)
-	} else {
-		conn, err = canopus.Dial(request.Gateway)
+	conf, err := GetConfig()
+	if err != nil {
+		result.err = ErrorNoConfig
+		c <- result
+		return
 	}
 
+	conn, err := canopus.DialDTLS(conf.Gateway, conf.Identity, conf.Passkey)
 	if err != nil {
 		result.err = err
 		c <- result
@@ -52,7 +50,7 @@ func _getRequest(request CoapRequest, c chan CoapResult) {
 
 	req := canopus.NewRequest(canopus.MessageConfirmable, canopus.Get)
 	req.SetStringPayload("Hello, canopus")
-	req.SetRequestURI(request.Uri)
+	req.SetRequestURI(URI)
 
 	resp, err := conn.Send(req)
 	if err != nil {
@@ -66,131 +64,133 @@ func _getRequest(request CoapRequest, c chan CoapResult) {
 	result.msg = resp.GetMessage().GetPayload()
 	c <- result
 }
+*/
 
-func _putRequest(request CoapRequest, c chan CoapResult) {
-	var result CoapResult
-
-	var conn canopus.Connection
-	var err error
-
-	if request.Identity != "" {
-		conn, err = canopus.DialDTLS(request.Gateway, request.Identity, request.Passkey)
-	} else {
-		conn, err = canopus.Dial(request.Gateway)
-
-	}
-
-	if err != nil {
-		result.err = err
-		c <- result
-		return
-	}
-
-	req := canopus.NewRequest(canopus.MessageConfirmable, canopus.Put)
-	req.SetRequestURI(request.Uri)
-	req.SetStringPayload(request.Payload)
-
-	resp, err := conn.Send(req)
-	if err != nil {
-		result.err = ErrorBadIdent
-		c <- result
-		return
-	}
-
-	result.msg = resp.GetMessage().GetPayload()
-	result.err = nil
-	c <- result
+func _request(params RequestParams) (retmsg coap.Message, err error) {
+	return params.req, nil
 }
 
-// Export GetRequestDTLS
-func GetRequestDTLS(gateway, uri, ident, key string) (msg canopus.MessagePayload, err error) {
-	c := make(chan CoapResult)
+func _requestDTLS(params RequestParams) (retmsg coap.Message, err error) {
+	mks := dtls.NewKeystoreInMemory()
+	dtls.SetKeyStores([]dtls.Keystore{mks})
+	mks.AddKey(params.id, []byte(params.key))
 
-	req := CoapRequest{Gateway: gateway, Uri: uri, Identity: ident, Passkey: key, Payload: ""}
-
-	go _getRequest(req, c)
-
-	select {
-	case res := <-c:
-		return res.msg, res.err
-	case <-time.After(time.Second * 5):
-		return nil, ErrorTimeout
-	}
-}
-
-// Export GetRequest
-func GetRequest(gateway, uri string) (msg canopus.MessagePayload, err error) {
-	return GetRequestDTLS(gateway, uri, "", "")
-}
-
-func PutRequestDTLS(gateway, uri, ident, key, payload string) (msg canopus.MessagePayload, err error) {
-	c := make(chan CoapResult)
-
-	req := CoapRequest{Gateway: gateway, Uri: uri, Identity: ident, Passkey: key, Payload: payload}
-
-	go _putRequest(req, c)
-
-	select {
-	case _ = <-c:
-		return GetRequestDTLS(gateway, uri, ident, key)
-	case <-time.After(time.Second * 5):
-		return nil, ErrorTimeout
-	}
-}
-
-func PutRequest(gateway, uri, payload string) (msg canopus.MessagePayload, err error) {
-	return PutRequestDTLS(gateway, uri, "", "", payload)
-}
-
-/* func Observe(URI string) {
-
-	conf, err := GetConfig()
-	if err != nil {
-		result.err = ErrorNoConfig
-		c <- result
-		return
-	}
-
-	conn, err := canopus.DialDTLS(conf.Gateway, conf.Identity, conf.Passkey)
-
-	tok, err := conn.ObserveResource("/15001/65540")
+	listner, err := dtls.NewUdpListener(":0", time.Second*900)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	obsChannel := make(chan canopus.ObserveMessage)
-	done := make(chan bool)
-	go conn.Observe(obsChannel)
+	peerParams := &dtls.PeerParams{
+		Addr:             fmt.Sprintf("%s:%d", params.host, params.port),
+		Identity:         params.id,
+		HandshakeTimeout: time.Second * 15}
 
-	notifyCount := 0
-	go func() {
-		for {
-			select {
-			case obsMsg, open := <-obsChannel:
-				if open {
-					if notifyCount == 5 {
-						fmt.Println("[CLIENT >> ] Canceling observe after 5 notifications..")
-						go conn.CancelObserveResource("watch/this", tok)
-						go conn.StopObserve(obsChannel)
-						done <- true
-						return
-					} else {
-						notifyCount++
-						// msg := obsMsg.Msg\
-						resource := obsMsg.GetResource()
-						val := obsMsg.GetValue()
+	peer, err := listner.AddPeerWithParams(peerParams)
+	if err != nil {
+		panic(err.Error())
+	}
 
-						fmt.Println("[CLIENT >> ] Got Change Notification for resource and value: ", notifyCount, resource, val)
-					}
-				} else {
-					done <- true
-					return
-				}
-			}
-		}
-	}()
-	<-done
-	fmt.Println("Done")
+	peer.UseQueue(true)
+
+	data, err := params.req.MarshalBinary()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = peer.Write(data)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	respData, err := peer.Read(time.Second)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	msg, err := coap.ParseMessage(respData)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = listner.Shutdown()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return msg, nil
 }
-*/
-func main() {}
+
+func _getRequest(params RequestParams, c chan coap.Message) {
+	params.req = coap.Message{
+		Type:      coap.Confirmable,
+		Code:      coap.GET,
+		MessageID: 1,
+	}
+
+	params.req.SetPathString(params.uri)
+
+	var msg coap.Message
+	var err error
+
+	if params.id != "" {
+		msg, err = _requestDTLS(params)
+	} else {
+		msg, err = _request(params)
+	}
+
+	if err != nil {
+		panic(err.Error())
+	}
+	c <- msg
+}
+
+func _putRequest(params RequestParams, c chan coap.Message) {
+	params.req = coap.Message{
+		Type:      coap.Confirmable,
+		Code:      coap.PUT,
+		MessageID: 1,
+		Payload:   []byte(params.payload),
+	}
+	params.req.SetPathString(params.uri)
+
+	var msg coap.Message
+	var err error
+
+	if params.id != "" {
+		msg, err = _requestDTLS(params)
+	} else {
+		msg, err = _request(params)
+	}
+	if err != nil {
+		panic(err.Error())
+	}
+	c <- msg
+}
+
+// GetRequest sends a default get
+func GetRequest(params RequestParams) (response []byte, err error) {
+	c := make(chan coap.Message)
+
+	go _getRequest(params, c)
+
+	select {
+	case res := <-c:
+		return res.Payload, nil
+	case <-time.After(time.Second * 60):
+		return nil, ErrorTimeout
+	}
+}
+
+// PutRequest sends a default Put-request
+func PutRequest(params RequestParams) (response []byte, err error) {
+	c := make(chan coap.Message)
+
+	go _putRequest(params, c)
+
+	select {
+	case res := <-c:
+		return res.Payload, nil
+	case <-time.After(time.Second * 5):
+		return nil, ErrorTimeout
+	}
+}
